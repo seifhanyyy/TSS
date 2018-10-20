777f814b4903caeb5dc00fd39a03debeb28dcc4a
seifhanyyy <43908969+seifhanyyy@users.noreply.github.com>
2018-10-20 21:38:46 +0200

    Add files via upload

---
+++ src/ticketingsystem/Category.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eslam
+ */
+public class Category extends Ticket{
+    private String Category; 
+
+    public String getCategory() {
+        return Category;
+    }
+
+    public void setCategory(String Category) {
+        this.Category = Category;
+    }
+    
+}
