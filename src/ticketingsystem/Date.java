6ab43e700e1b5a34eaebac44354948f4ed882cfd
seifhanyyy <43908969+seifhanyyy@users.noreply.github.com>
2018-10-21 22:12:14 +0200

    Add files via upload

---
+++ src/ticketingsystem/Date.java
@@ -0,0 +1,58 @@
+package ticketingsystem;
+
+public class Date {
+private int Day;
+private int Month;
+private int Year;
+private int hour;
+private int minute;
+private int seconds;
+
+    public int getDay() {
+        return Day;
+    }
+
+    public void setDay(int Day) {
+        this.Day = Day;
+    }
+
+    public int getMonth() {
+        return Month;
+    }
+
+    public void setMonth(int Month) {
+        this.Month = Month;
+    }
+
+    public int getYear() {
+        return Year;
+    }
+
+    public void setYear(int Year) {
+        this.Year = Year;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+}
---
+++ src/ticketingsystem/SolvedTicket.java
@@ -0,0 +1,16 @@
+package ticketingsystem;
+
+public class SolvedTicket extends Date{
+Date TimeTakenToBeSolved;
+
+    public Date getTimeTakenToBeSolved() {
+        return TimeTakenToBeSolved;
+    }
+
+    public void setTimeTakenToBeSolved(Date TimeTakenToBeSolved) {
+        this.TimeTakenToBeSolved = TimeTakenToBeSolved;
+    }
+public void SolvedTickets(){;
+}
+
+}
