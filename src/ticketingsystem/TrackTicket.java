02e9e31efb06c6332b6383ae0e2b7ab981b384dc
Mohamed-Abdelhamed <43310877+Mohamed-Abdelhamed@users.noreply.github.com>
2018-10-21 22:31:56 +0200

    Update TrackTicket.java

--- src/ticketingsystem/TrackTicket.java
+++ src/ticketingsystem/TrackTicket.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project;
 
-/**
- *
- * @author Dr.Bony George
- */
+
 public class TrackTicket implements CRUD {
     
     private int id ;
