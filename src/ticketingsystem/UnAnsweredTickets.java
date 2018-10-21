f2cfe62c472169003e8d6f4285e8fc24980510c7
Mohamed-Abdelhamed <43310877+Mohamed-Abdelhamed@users.noreply.github.com>
2018-10-21 22:22:18 +0200

    Add files via upload

---
+++ src/ticketingsystem/UnAnsweredTickets.java
@@ -0,0 +1,16 @@
+package ticketingsystem;
+
+public class UnAnsweredTickets extends Date {
+private Date DueDate;
+
+    public Date getDueDate() {
+        return DueDate;
+    }
+
+    public void setDueDate(Date DueDate) {
+        this.DueDate = DueDate;
+    }
+
+public void UnAnsweredTickets(){
+    
+}}
