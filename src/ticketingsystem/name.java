5d2482fae51fd2cdd5e28afc45af95a14a559cd4
Mohamed-Abdelhamed <43310877+Mohamed-Abdelhamed@users.noreply.github.com>
2018-10-23 20:29:14 +0200

    Add files via upload

---
+++ src/ticketingsystem/name.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eslam
+ */
+public class name {
+    private String FirstName;
+    private String LastName;
+    private String MiddleName;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getMiddleName() {
+        return MiddleName;
+    }
+
+    public void setMiddleName(String MiddleName) {
+        this.MiddleName = MiddleName;
+    }
+}
