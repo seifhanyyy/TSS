4e0dd3a05a58b870f387ae03fdb49b45dfaf8ca8
Mohamed-Abdelhamed <43310877+Mohamed-Abdelhamed@users.noreply.github.com>
2018-10-24 18:56:11 +0200

    Add files via upload

---
+++ src/ticketingsystem/FilterTicket.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication12;
+
+/**
+ *
+ * @author Dr.Bony George
+ */
+public class FilterTicket {
+    
+    private Name name;
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+    public void View(){
+     
+        
+        
+    }
+    public Boolean Search(Ticket ticket)
+    {
+    
+    s
+    }
+    
+}
---
+++ src/ticketingsystem/UserPortal.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication12;
+
+/**
+ *
+ * @author Dr.Bony George
+ */
+public class UserPortal{
+   private Name name;
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+    
+public Boolean Search(Ticket ticket){
+        
+   
+}
+}
